51c16b45e47cfdd50a7695501c8c93387b522028
KevinReynolds1211 <kevinreynolds1@mail.weber.edu>
2020-05-26 16:15:57 -0600

    initial commit

---
+++ MackTechGroupProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MackTechGroupProject.MvcApplication" Language="C#" %>
---
+++ MackTechGroupProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MackTechGroupProject.Startup))]
+namespace MackTechGroupProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
