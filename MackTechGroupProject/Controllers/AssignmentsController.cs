7b7a785ce44c130557be980d4fc0ffa8520c7a32
KevinReynolds1211 <kevinreynolds1@mail.weber.edu>
2020-06-04 19:24:16 -0600

    added controllers and views for dummy pages when links are clicked on the home page.

---
+++ MackTechGroupProject/Controllers/AssignmentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MackTechGroupProject.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        // GET: Assignments
+        public ActionResult Assignment1()
+        {
+            return View();
+        }
+        public ActionResult Assignment2()
+        {
+            return View();
+        }
+        public ActionResult Assignment3()
+        {
+            return View();
+        }
+        public ActionResult Discussion()
+        {
+            return View();
+        }
+    }
+}
---
+++ MackTechGroupProject/Controllers/CoursesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MackTechGroupProject.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult CS3620()
+        {
+            return View();
+        }
+
+        public ActionResult CS3750()
+        {
+            return View();
+        }
+
+        public ActionResult MATH1220()
+        {
+            return View();
+        }
+
+        public ActionResult PHYS2120()
+        {
+            return View();
+        }
+
+        //**OTHER SECTIONS OF COURSES**
+
+        public ActionResult Grades()
+        {
+            return View();
+        }
+    }
+}
