8c909f60871a861e1101d73d07e8768a2953ee93
Nicholas Johnson <ntj117@outlook.com>
2020-06-16 20:21:49 -0600

    Add Course Db Context

---
+++ MackTechGroupProject/Models/CoursesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MackTechGroupProject.Models
+{
+    public class CoursesContext : DbContext
+    {
+        public CoursesContext() : base()
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ MackTechGroupProject/Models/CoursesModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MackTechGroupProject.Models
+{
+   /// <summary>
+   /// This is the course model class that creates the scaffolding for the database
+   /// </summary>
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public int InstructorID { get; set; }
+        public int CreditHours { get; set; }
+    }
+
+    /// <summary>
+    /// For future use if needed
+    /// </summary>
+    //public class StudentCourse
+    //{
+
+    //}
+}
