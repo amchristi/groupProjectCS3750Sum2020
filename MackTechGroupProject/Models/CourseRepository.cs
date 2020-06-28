a6c669961b59ed297e1cadb651b111923a312b9a
Chelsea Throne <chelseathrone@mail.weber.edu>
2020-06-28 13:21:36 -0600

    Added MSTest to Solution

---
+++ MackTechGroupProject/Models/CourseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MackTechGroupProject.Models
+{
+    public class CourseRepository
+    {
+
+        private static List<Course> courses = new List<Course>();
+
+        public static IEnumerable<Course> Courses
+        {
+            get
+            {
+                return courses;
+            }
+        }
+
+        public static void AddCourse(Course course)
+        {
+            courses.Add(course);
+        }
+
+    }
+}
