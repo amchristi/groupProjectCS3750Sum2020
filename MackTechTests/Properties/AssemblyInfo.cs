52fa06994050fa3cd952adc1eeb91d1803d28da7
Chelsea Throne <chelseathrone@mail.weber.edu>
2020-07-15 11:37:38 -0600

    add functioning .net framework test from prof

---
+++ MackTechTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MackTechTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MackTechTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7fc5eba-ab3a-4928-84a2-7ebadd4d3aac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MackTechTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MackTechTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var _context = new MackTechGroupProject.Models.ApplicationDbContext();
+            var y = _context.Assignments.Where(x => x.Points > 10);
+            System.Diagnostics.Debug.WriteLine(y.Count());
+            Assert.IsTrue(y.Count() > 0);
+
+        }
+    }
+}
