7fd87e1f0f3a0eb56f4faf578c766195b91af25c
Andrew Hammon <andrewhammon@mail.weber.edu>
2020-07-21 19:12:43 -0600

    Instructor grading working correctly.

--- MackTechGroupProject/Controllers/AssignmentsController.cs
+++ MackTechGroupProject/Controllers/AssignmentsController.cs
@@ -263,5 +263,29 @@ namespace MackTechGroupProject.Controllers
             return RedirectToAction("GradeAssignment", "Assignment", new { id = assignmentId });
         }
 
+
+
+
+
+
+        public ActionResult DownloadSubmittedAssignemnt(string filePath)
+        {
+            string fullName = Server.MapPath("~" + filePath);
+
+            byte[] fileBytes = GetFile(fullName);
+            return File(
+                fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
+        }
+
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
+        }
+
     }
 }
