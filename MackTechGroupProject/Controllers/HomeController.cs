d548ce1adb203966405076a9265a630fb48c4491
Montana Throne <chelseathrone@mail.weber.edu>
2020-08-03 15:34:23 -0600

    clean database and update home screen for instructor

--- MackTechGroupProject/Controllers/HomeController.cs
+++ MackTechGroupProject/Controllers/HomeController.cs
@@ -18,15 +18,29 @@ namespace MackTechGroupProject.Controllers
             var context = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
             var userId = User.Identity.GetUserId();
 
+            //student - get all assignments from enrollments
             var currentEnrollmentsWithAssignments = context.Enrollments.Where(x => x.User.Id == userId).Include(x => x.User).Include(x => x.Course).Include("Course.Assignments").ToList();
+            
+            //student - get all upcoming assignments and sort them by due date
             var allAssignments = currentEnrollmentsWithAssignments.Select(x => x.Course).SelectMany(y => y.Assignments).ToList();
             var allRelevantAssignments = allAssignments.Where(x => x.DueDate > DateTime.Now.Date).ToList();
             var allAssignmentsSorted = allRelevantAssignments.OrderBy(x => x.DueDate).ToList();
 
+            //instructor - get all courses
+            var currentInstructor = context.Users.Where(x => x.Id == userId).FirstOrDefault();
+            var currentInstructorCourses = context.Courses.Where(x => x.Instructor.Id == currentInstructor.Id).ToList();
+
+            //instructor - get all upcoming assignments from those courses and sort them by due date
+            var allInstructorAssignments = currentInstructorCourses.SelectMany(y => y.Assignments).ToList();
+            var allRelevantInstructorAssignments = allInstructorAssignments.Where(x => x.DueDate > DateTime.Now.Date).ToList();
+            var allRelevantInstructorAssignmentsSorted = allRelevantInstructorAssignments.OrderBy(x => x.DueDate).ToList();
+
             var toDoListViewModel = new ToDoListViewModel()
             {
                 currentAssignmentsView = allAssignmentsSorted,
-                currentEnrollmentsView = currentEnrollmentsWithAssignments
+                currentEnrollmentsView = currentEnrollmentsWithAssignments,
+                currentCoursesView = currentInstructorCourses,
+                currentInstructorAssignmentsView = allRelevantInstructorAssignmentsSorted
             };
 
             return View(toDoListViewModel);
